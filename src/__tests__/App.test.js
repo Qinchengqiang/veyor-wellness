6a1894504c4048f1cf8393aeb39e7cb8ffd63953
Qinchengqiang <steven.qin90@gmail.com>
2021-08-11 17:23:17 +1000

    launching setting

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react';  // testing-library
+import {cleanup, render, screen} from '@testing-library/react';  // testing-library
 import renderer from 'react-test-renderer';
 import React from 'react';
 import App from '../components/App';
@@ -11,10 +11,14 @@ import {Provider} from "react-redux";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 /**
- * add it() or test() blocks with the name of the test and its code. You may optionally wrap them in describe() blocks for logical grouping;
+ * add it() or test() blocks with the name of the test and its code;
+ *
+ * You may optionally wrap them in describe() blocks for logical grouping;
+ *
  * Jest provides a built-in expect() global function for making assertions;
  *
  * All expect() matchers supported by Jest.
+ *
  * You can also use jest.fn() and expect(fn).toBeCalled() to create “spies” or mock functions
  *
  */
@@ -22,57 +26,63 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 
 /** must wrap the app component as we use redux */
 const AppWrapper = () => {
-  const store = createStore(
-      rootReducer,
-      composeWithDevTools(
-          applyMiddleware(thunk, logger)
-      )
-  );
+    const store = createStore(
+        rootReducer,
+        composeWithDevTools(
+            applyMiddleware(thunk, logger)
+        )
+    );
 
-  const routes = (
-      <>
-        <Route exact path="/" component={App}/>
-      </>);
+    const routes = (
+        <>
+            <Route exact path="/" component={App}/>
+        </>);
 
-  // axios default baseURL
-  // if (process.env.NODE_ENV === "production") {
-  //     axios.defaults.baseURL = "https://interview-web-service.mountainpass.com.au/";
-  // }
-  //
-  // if (localStorage.jwtToken) {
-  //     setAxiosAuth(user);
-  //     store.dispatch(setCurrentUser(user))    // keep login status after re-loading page
-  // }
+    // axios default baseURL
+    // if (process.env.NODE_ENV === "production") {
+    //     axios.defaults.baseURL = "https://interview-web-service.mountainpass.com.au/";
+    // }
+    //
+    // if (localStorage.jwtToken) {
+    //     setAxiosAuth(user);
+    //     store.dispatch(setCurrentUser(user))    // keep login status after re-loading page
+    // }
 
-  return (
-      <Provider store={store}>
-        <Router>
-          {routes}
-        </Router>
-      </Provider>
-  )
+    return (
+        <Provider store={store}>
+            <Router>
+                {routes}
+            </Router>
+        </Provider>
+    )
 }
 
-// test 1   (using render from @testing-library)
-it('App test 1: render app', () => {
-    render(<AppWrapper/>);
-    const AppEle = screen.getByTestId('App');   // attr in element: data-testid={`App`}
-    expect(AppEle).toBeInTheDocument();
-})
-
-// test 2   (using render from @testing-library)
-test('App test 2: hello', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/hello react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(() => {
+    cleanup();
 });
+
+describe('App group:', () => {
+    // test 1   (using render from @testing-library)
+    it('App test 1: render app', () => {
+        render(<AppWrapper/>);
+        const AppEle = screen.getByTestId('App');   // attr in element: data-testid={`App`}
+        expect(AppEle).toBeInTheDocument();
+    })
+
+    // test 2   (using render from @testing-library)
+    test('App test 2: hello', () => {
+        render(<App/>);
+        const linkElement = screen.getByText(/hello react/i);
+        expect(linkElement).toBeInTheDocument();
+    });
+})
 
 
 /**
  * snapshot test (using react-test-renderer)
  */
 test('App test: matches snapshot', () => {
-  const tree = renderer.create(<AppWrapper/>).toJSON();
-  expect(tree).toMatchSnapshot();   // failed，press ‘u’ to update. (jest -u)
+    const tree = renderer.create(<AppWrapper/>).toJSON();
+    expect(tree).toMatchSnapshot();   // failed，press ‘u’ to update. (jest -u)
 })
 
