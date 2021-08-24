eb6acad80904097f91b043c1ea3c9eec8ccbe884
Qinchengqiang <steven.qin90@gmail.com>
2021-08-24 16:16:01 +1000

    use useSelector in index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import '../assets/styles/App.scss';
+import {useSelector, shallowEqual} from "react-redux";
 
 function App() {
+    const authRedux = useSelector(state => state.auth, shallowEqual)
+
     return (
         <div className="App" data-testid={`App`}>
             <h3>hello</h3>
+            {authRedux.isAuthenticated ? <p>{authRedux.username}</p>
+                : <p>{`login first ...`}</p> }
         </div>
     );
 }
