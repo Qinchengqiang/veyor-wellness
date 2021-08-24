169fd3a8f9f9e34c74ad8ae678c6bf975afff755
Qinchengqiang <steven.qin90@gmail.com>
2021-08-24 16:33:53 +1000

    use useDispatch in index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import '../assets/styles/App.scss';
-import {useSelector, shallowEqual} from "react-redux";
+import {useSelector, useDispatch, shallowEqual} from "react-redux";
+import {login} from '../actions/loginAction';
 
 function App() {
     const authRedux = useSelector(state => state.auth, shallowEqual)
+    const dispatch = useDispatch();
 
     return (
         <div className="App" data-testid={`App`}>
             <h3>hello</h3>
-            {authRedux.isAuthenticated ? <p>{authRedux.username}</p>
-                : <p>{`login first ...`}</p> }
+            {authRedux.isAuthenticated ?
+                <h4>{authRedux.user.username}</h4>
+                :
+                <p>
+                    <button onClick={()=>dispatch(login())}>login</button>
+                </p>}
         </div>
     );
 }
